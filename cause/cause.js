67829ac1b864edd2b9dbb12745d63d9c81ba75ea
egasepyana <egasepyana04@gmail.com>
2025-11-25 01:13:21 +0700

    fix: :ambulance: project structure

--- cause/cause.js
+++ cause/cause.js
@@ -83,7 +83,7 @@ function displayNewReason() {
                             opacity: 0,
                             duration: 1,
                             onComplete: () => {
-                                window.location.href = 'timeline.html'; // Replace with the actual URL of the next page
+                                window.location.href = '../timeline/timeline.html'; // Replace with the actual URL of the next page
                             }
                         });
                     });
--- script.js
+++ script.js
@@ -89,7 +89,7 @@ window.addEventListener('load', () => {
                 opacity: 0,
                 duration: 1,
                 onComplete: () => {
-                    window.location.href = 'cause.html'; // Replace with the actual URL of the next page
+                  window.location.href = "cause/cause.html"; // Replace with the actual URL of the next page
                 }
             });
         });
